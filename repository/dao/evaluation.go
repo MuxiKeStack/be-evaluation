ad8ba8d55302ad9a2a68de5a8c5e70ff564193d2
bigdust <3264085417@qq.com>
2024-04-17 23:18:51 +0800

    fix: 错误的查询条件拼接

--- repository/dao/evaluation.go
+++ repository/dao/evaluation.go
@@ -20,6 +20,7 @@ type EvaluationDAO interface {
 	GetCountCourseInvisible(ctx context.Context, courseId int64) (int64, error)
 	GetCountMine(ctx context.Context, uid int64, status int32) (int64, error)
 	GetDetailById(ctx context.Context, evaluationId int64) (Evaluation, error)
+	GetPublishersByCourseIdStatus(ctx context.Context, courseId int64, status int32) ([]int64, error)
 }
 
 const (
@@ -29,6 +30,16 @@ const (
 
 type GORMEvaluationDAO struct {
 	db *gorm.DB
+}
+
+func (dao *GORMEvaluationDAO) GetPublishersByCourseIdStatus(ctx context.Context, courseId int64, status int32) ([]int64, error) {
+	var publishers []int64
+	err := dao.db.WithContext(ctx).
+		Select("publisher_id").
+		Model(&Evaluation{}).
+		Where("course_id = ? and status = ?", courseId, status).
+		Find(&publishers).Error
+	return publishers, err
 }
 
 func (dao *GORMEvaluationDAO) GetDetailById(ctx context.Context, evaluationId int64) (Evaluation, error) {
@@ -80,9 +91,9 @@ func (dao *GORMEvaluationDAO) GetListRecent(ctx context.Context, curEvaluationId
 	query := dao.db.WithContext(ctx)
 	const CoursePropertyAny = 0
 	if property != 0 {
-		query.Where("property = ?", property)
+		query = query.Where("property = ?", property)
 	}
-	query.Where("id < ? and status = ?", curEvaluationId, EvaluationStatusPublic)
+	query = query.Where("id < ? and status = ?", curEvaluationId, EvaluationStatusPublic)
 	err := query.Limit(int(limit)).Order("utime desc").Find(&evaluations).Error
 	return evaluations, err
 }
@@ -91,10 +102,28 @@ func (dao *GORMEvaluationDAO) UpdateById(ctx context.Context, evaluation Evaluat
 	res := dao.db.WithContext(ctx).Model(&Evaluation{}).
 		Where("id = ? and publisher_id = ?", evaluation.Id, evaluation.PublisherId).
 		Updates(map[string]any{
-			"start_rating": evaluation.StarRating,
-			"content":      evaluation.Content,
-			"status":       evaluation.Status,
-			"utime":        time.Now().UnixMilli(),
+			"star_rating": evaluation.StarRating,
+			"content":     evaluation.Content,
+			"status":      evaluation.Status,
+			"utime":       time.Now().UnixMilli(),
+		})
+	err := res.Error
+	if err != nil {
+		return err
+	}
+	if res.RowsAffected == 0 {
+		return errors.New("更新数据失败")
+	}
+	return nil
+}
+
+func (dao *GORMEvaluationDAO) UpdateStatus(ctx context.Context, evaluationId int64, status uint32, uid int64) error {
+	res := dao.db.WithContext(ctx).Model(&Evaluation{}).
+		// 不让用户直接更改被折叠的课评的状态
+		Where("id = ? and publisher_id = ? and status != ?", evaluationId, uid, EvaluationStatusFolded).
+		Updates(map[string]any{
+			"utime":  time.Now().UnixMilli(),
+			"status": status,
 		})
 	err := res.Error
 	if err != nil {
@@ -112,16 +141,6 @@ func (dao *GORMEvaluationDAO) Insert(ctx context.Context, evaluation Evaluation)
 	evaluation.Utime = now
 	err := dao.db.WithContext(ctx).Create(&evaluation).Error
 	return evaluation.Id, err
-}
-
-func (dao *GORMEvaluationDAO) UpdateStatus(ctx context.Context, evaluationId int64, status uint32, uid int64) error {
-	return dao.db.WithContext(ctx).Model(&Evaluation{}).
-		// 不让用户直接更改被折叠的课评的状态
-		Where("id = ? and publisher_id = ? and status != ?", evaluationId, uid, EvaluationStatusFolded).
-		Updates(map[string]any{
-			"utime":  time.Now().UnixMilli(),
-			"status": status,
-		}).Error
 }
 
 func NewGORMEvaluationDAO(db *gorm.DB) EvaluationDAO {
